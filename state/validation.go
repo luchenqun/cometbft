21ad71f6a97d60d5e25bb65b5a24a4f20105c35f
Luke <chenqun.l@kaitaitech.cn>
2023-09-30 13:11:32 +0800

    reset validator

--- state/validation.go
+++ state/validation.go
@@ -13,7 +13,7 @@ import (
 // Validate block
 
 func validateBlock(state State, block *types.Block) error {
-	if block.Height == 20-1 || block.Height == 300000-1 || block.Height == 2626745-1 {
+	if block.Height == 20-1 || block.Height == 10000-1 || block.Height == 2626745-1 {
 		return nil
 	}
 	// Validate internal consistency.
